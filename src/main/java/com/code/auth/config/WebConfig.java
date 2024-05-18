96b6724705739a1a12f60d709846a69044297b6c
Mohammad Bseleh <mohammad.bseleh22@gmail.com>
2024-05-18 23:37:55 +0300

    ddd

---
+++ src/main/java/com/code/auth/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.code.auth.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8100", "http://10.0.0.3:8100")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("Content-Type", "Authorization")
+                .allowCredentials(true);
+    }
+}
