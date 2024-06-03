81bf3b33f38ac0ac785eb9a113fc67daff5c1130
Mohammad Bseleh <mohammad.bseleh22@gmail.com>
2024-06-04 00:03:02 +0300

    ddd

--- src/main/java/com/code/auth/config/WebSocketConfig.java
+++ src/main/java/com/code/auth/config/WebSocketConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.*;
-import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
 
 @Configuration
 @EnableWebSocket
@@ -18,8 +17,7 @@ public class WebSocketConfig implements WebSocketConfigurer {
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(myWebSocketHandler, "/ws/chat")
-                .addInterceptors(new HttpSessionHandshakeInterceptor())
+        registry.addHandler(myWebSocketHandler, "/chat")
                 .setAllowedOrigins("*");
     }
 }
